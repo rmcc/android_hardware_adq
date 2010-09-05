143f2c00577a2f280aadc181b20bc8936dd6c2dc
rmcc <github@cerqueira.org>
2010-09-05 15:21:28 +0100

    Move sensors to hardware/adq, add libcamera to same repository

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),adq)
-  include $(call all-subdir-makefiles)
-endif
+
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation, not prelinked and stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
+include $(CLEAR_VARS)
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SRC_FILES := bma020.c
+LOCAL_MODULE := sensors.default
+include $(BUILD_SHARED_LIBRARY)
